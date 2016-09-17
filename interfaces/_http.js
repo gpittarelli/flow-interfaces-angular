6192e8bee044fbfb02d9e7be5149f698407cd888
George Pittarelli <tf2stadium@gjp.cc>
2016-09-17 02:15:57 -0700

    Add http response type

--- interfaces/_http.js
+++ interfaces/_http.js
@@ -1,8 +1,15 @@
 /* jshint ignore: start */
+type AngularJS_HTTPResponse = {
+  data: string | Object;
+  status: number;
+  headers(headerName: string): string;
+  config: Object;
+  statusText: string
+}
+
 declare class AngularJS_HTTP {
-  get(url: string): Promise;
-  get(url: string, config: Object): Promise;
+  get(url: string): Promise<AngularJS_HTTPResponse>;
+  get(url: string, config: Object): Promise<AngularJS_HTTPResponse>;
 }
 
 declare var $http: AngularJS_HTTP;
-
